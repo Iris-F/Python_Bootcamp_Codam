b6c7ab78ad541e99af44f970c4d66309b4fbac06
Iris Fehse <irisfehse@gmail.com>
2025-07-04 09:59:08 +0200

    Added code for count_it.py

--- module7/ex01/count_it.py
+++ module7/ex01/count_it.py
@@ -2,3 +2,10 @@
 import sys
 import re
 
+
+if len(sys.argv) > 1:
+    print("Parameters: ", len(sys.argv)-1)
+    for i in range(len(sys.argv)):
+        print(sys.argv[i])
+else:
+    print("none")
