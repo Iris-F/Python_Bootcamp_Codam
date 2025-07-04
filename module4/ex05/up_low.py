7fce288073565c454e88955ca9e56806ccfea98c
Iris Fehse <irisfehse@gmail.com>
2025-07-04 11:44:09 +0200

    Added swapcase method to module 5, exercise 5 instead of loop and string index

--- module4/ex05/up_low.py
+++ module4/ex05/up_low.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
 word = input("Enter a string:\n")
-i = 0
-new_word = ""
 
-while i < len(word):
-    if word[i].isupper():
-        new_word += word[i].lower()
-    else:
-        new_word += word[i].upper()
-    i += 1
-print(new_word)
+#much simpler solution with swapcase:
+print(word.swapcase())
+
+# i = 0
+# new_word = ""
+
+# while i < len(word):
+#     if word[i].isupper():
+#         new_word += word[i].lower()
+#     else:
+#         new_word += word[i].upper()
+#     i += 1
+# print(new_word)
